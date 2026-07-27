2ace2ea7d2e5a58fafc81d9ed57f7627db02d95b
moudgeosom <moudgeosom@gmail.com>
2026-07-27 10:36:09 +0300

    mise a jour de 27/07/2026

--- backend/src/models/Order.model.js
+++ backend/src/models/Order.model.js
@@ -29,6 +29,7 @@ const orderSchema = new mongoose.Schema({
   profit_amount: { type: Number, default: 0 },
   total: { type: Number, default: 0 },
   ci_amount: { type: Number, default: 0 },
+  complete_document: { type: String, default: '' },
   order_date: { type: Date, default: Date.now },
   delivery_status: { type: String, default: 'PENDING' },
   status: { type: String, default: 'PENDING' },
